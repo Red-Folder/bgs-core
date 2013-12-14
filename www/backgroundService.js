0ac9b656ee7ace5628e00f2a8a5f21356e57980d
Red-Folder <markbryantaylor@gmail.com>
2013-12-14 22:04:22 +0000

    small changes

--- www/backgroundService.js
+++ www/backgroundService.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Red Folder Consultancy Ltd
+ * Copyright 2013 Red Folder Consultancy Ltd
  *   
  * Licensed under the Apache License, Version 2.0 (the "License");   
  * you may not use this file except in compliance with the License.   
@@ -17,155 +17,209 @@
 /*
  * Constructor
  */
-function BackgroundService(serviceName) {
-	var ServiceName = serviceName;
+function CreateBackgroundService(serviceName, require, exports, module) {
+	var exec = require("cordova/exec");
 	
-	this.getServiceName = function() {
-		return ServiceName;
+	var BackgroundService = function (serviceName) {
+		var ServiceName = serviceName
+		this.getServiceName = function() {
+			return ServiceName;
+		};
 	};
-};
 
-/**
-  * All methods attempt to return the following data in both the success and failure callbacks
-  * Front end development should take into account any all or all of these values may be null
-  *
-  * Following returned in the JSONObject:
-  *		Boolean Success - was the call a success
-  *		int ErrorCode - Error code if an error occurred, else will be zero
-  *		String ErrorMessage - Text representation of the error code
-  *		Boolean ServiceRunning - True if the Service is running
-  *		Boolean TimerEnabled - True if the Timer is enabled
-  *		Boolean RegisteredForBootStart - True if the Service is registered for boot start
-  *		JSONObject Configuration - A JSONObject of the configuration of the service (contents dependant on the service)
-  *		JSONObject LastestResult - A JSONObject of the last result of the service (contents dependant on the service)
-  */
+	var BackgroundServiceError = function (code, message) {
+		this.code = code || null;
+		this.message = message || null;
+	};	
 
-/**
-  * Starts the Service
-  * 
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.startService = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'startService',      
-							[this.getServiceName()]);
-};
+	/**
+	  * All methods attempt to return the following data in both the success and failure callbacks
+	  * Front end development should take into account any all or all of these values may be null
+	  *
+	  * Following returned in the JSONObject:
+	  *		Boolean Success - was the call a success
+	  *		int ErrorCode - Error code if an error occurred, else will be zero
+	  *		String ErrorMessage - Text representation of the error code
+	  *		Boolean ServiceRunning - True if the Service is running
+	  *		Boolean TimerEnabled - True if the Timer is enabled
+	  *		Boolean RegisteredForBootStart - True if the Service is registered for boot start
+	  *		JSONObject Configuration - A JSONObject of the configuration of the service (contents dependant on the service)
+	  *		JSONObject LastestResult - A JSONObject of the last result of the service (contents dependant on the service)
+	  *		int TimerMilliseconds - Milliseconds used by the background service if Timer enabled
+	  *		Boolean RegisteredForUpdates - True if the Service is registered to send updates to the front-end
+	  */
 
-/**
-  * Stops the Service
-  *
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.stopService = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'stopService',      
-							[this.getServiceName()]);
-};
+	/**
+	  * Starts the Service
+	  * 
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.startService = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'startService',      
+						[this.getServiceName()]);
+	};
 
-/**
-  * Enables the Service Timer
-  *
-  * @param milliseconds The milliseconds used for the timer
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.enableTimer = function(milliseconds, successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'enableTimer',      
-							[this.getServiceName(), milliseconds]);
-};
+	/**
+	  * Stops the Service
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.stopService = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'stopService',      
+						[this.getServiceName()]);
+	};
 
-/**
-  * Disabled the Service Timer
-  *
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.disableTimer = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'disableTimer',      
-							[this.getServiceName()]);
-};
+	/**
+	  * Enables the Service Timer
+	  *
+	  * @param milliseconds The milliseconds used for the timer
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.enableTimer = function(milliseconds, successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'enableTimer',      
+						[this.getServiceName(), milliseconds]);
+	};
 
-/**
-  * Sets the configuration for the service
-  *
-  * @oaran configuration JSONObject to be sent to the service
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.setConfiguration = function(configuration, successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'setConfiguration',      
-							[this.getServiceName(), configuration]);
-};
+	/**
+	  * Disabled the Service Timer
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.disableTimer = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'disableTimer',      
+						[this.getServiceName()]);
+	};
 
-/**
-  * Registers the service for Boot Start
-  *
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.registerForBootStart = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'registerForBootStart',      
-							[this.getServiceName()]);
-};
+	/**
+	  * Sets the configuration for the service
+	  *
+	  * @param configuration JSONObject to be sent to the service
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.setConfiguration = function(configuration, successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'setConfiguration',      
+						[this.getServiceName(), configuration]);
+	};
 
-/**
-  * Deregisters the service for Boot Start
-  *
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.deregisterForBootStart = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'deregisterForBootStart',      
-							[this.getServiceName()]);
-};
+	/**
+	  * Registers the service for Boot Start
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.registerForBootStart = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'registerForBootStart',      
+						[this.getServiceName()]);
+	};
 
-/**
-  * Get the current status of the service.
-  * 
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.isRegisteredForBootStart = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'isRegisteredForBootStart',      
-							[this.getServiceName()]);
-};
+	/**
+	  * Deregisters the service for Boot Start
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.deregisterForBootStart = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'deregisterForBootStart',      
+						[this.getServiceName()]);
+	};
+
+	/**
+	  * Get the current status of the service.	
+	  * 
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.isRegisteredForBootStart = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'isRegisteredForBootStart',      
+						[this.getServiceName()]);
+	};
 
 
-/**
-  * Returns the status of the service
-  *
-  * @param successCallback The callback which will be called if the method is successful
-  * @param failureCallback The callback which will be called if the method encounters an error
-  */
-BackgroundService.prototype.getStatus = function(successCallback, failureCallback) { 
-	return cordova.exec(	successCallback,      
-							failureCallback,      
-							'BackgroundServicePlugin',      
-							'getStatus',      
-							[this.getServiceName()]);
-};
+	/**
+	  * Returns the status of the service
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.getStatus = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'getStatus',      
+						[this.getServiceName()]);
+	};
 
+	/**
+	  * Returns the doWork once
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.runOnce = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'runOnce',      
+						[this.getServiceName()]);
+	};
+
+	/**
+	  * Registers for doWork() updates
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.registerForUpdates = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'registerForUpdates',      
+						[this.getServiceName()]);
+	};
+
+	/**
+	  * Deregisters for doWork() updates
+	  *
+	  * @param successCallback The callback which will be called if the method is successful
+	  * @param failureCallback The callback which will be called if the method encounters an error
+	  */
+	BackgroundService.prototype.deregisterForUpdates = function(successCallback, failureCallback) { 
+		return exec(	successCallback,      
+						failureCallback,      
+						'BackgroundServicePlugin',      
+						'deregisterForUpdates',      
+						[this.getServiceName()]);
+	};
+
+	var backgroundService = new BackgroundService(serviceName);
+	module.exports = backgroundService;
+}; 
